c0261cfe9a5eb1132942541c65787090638863b7
Innocenzo Merola <enzomerola02@gmail.com>
2024-01-31 23:40:58 +0100

    change

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+const container = document.getElementById("container");
+
 const aggiungiAllaLista = document.getElementById("singleInput");
 
 const button = document.getElementById("btn");
@@ -15,5 +17,20 @@ button.onclick = function () {
     console.log(list);
     list.appendChild(li);
     singleInput.value = "";
+
+    const btnDel = document.createElement("button");
+    btnDel.classList.add("trashBtn");
+    btnDel.innerHtml = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/><path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/></svg>`;
+    li.appendChild(btnDel);
+    btnDel.addEventListener("click", deleteLi);
+    btnDel.onclick = function (e) {
+      console.log("btn del target", e.target);
+    };
+  }
+  container.appendChild(list);
+
+  function deleteLi(e) {
+    const shoppingCart = e.currentTarget.parentElement;
+    list.removeChild(shoppingCart);
   }
 };
